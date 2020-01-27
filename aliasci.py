9224a0eb781600c85383014c00373ab58eb108c3
Armin <becherarmin@gmail.com>
2020-01-27 22:05:13 +0100

    Read toml file

---
+++ aliasci.py
@@ -0,0 +1,30 @@
+import argparse
+import sys
+import toml
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description="Convert a cli toml file to different command line scripts used to permanently set an alias for different tools")
+    parser.add_argument('config_file', type=argparse.FileType(encoding='UTF-8'),
+                        help='The config file containing a list of command line arguments')
+
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        sys.exit(1)
+    return args
+
+
+def main():
+    args = parse_args()
+
+    parsed_toml = toml.loads(args.config_file.read())
+
+    # TODO Run script
+    print(parsed_toml)
+
+
+if __name__ == '__main__':
+    main()
