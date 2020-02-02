5f37562ccdbd0dd52154bff9136fa694ecbb4f15
Armin <becherarmin@gmail.com>
2020-02-02 18:53:32 +0100

    Set aliases ps

--- aliasci.py
+++ aliasci.py
@@ -32,6 +32,9 @@ def parse_args():
 
 def load_toml_config(toml_string):
     toml_json = toml.loads(toml_string)
+    if 'all' not in toml_json:
+        eprint(f"The [all] section is missing in the toml file.")
+        sys.exit(1)
     possible_keys = [e.value for e in ConsoleType]
     possible_keys.append("all")
     for key in toml_json:
@@ -41,13 +44,28 @@ def load_toml_config(toml_string):
     return toml_json
 
 
+def _merge(config, key):
+    if key in config:
+        return {**config['all'], **config[key]}
+    return config['all']
+
+
 def main():
     args = parse_args()
 
     config = load_toml_config(args.config_file.read())
 
+    ps_script = generate_ps(_merge(config, 'ps'))
+
     # TODO Run script
-    print(config)
+    print(ps_script)
+
+
+def generate_ps(config):
+    script = str()
+    for (key, value) in config.items():
+        script += f'Set-Alias -Name {key} -Value {value}\n'
+    return script
 
 
 if __name__ == '__main__':
