f5687b718ec3eb0d727583be7601526485174db3
Armin Becher <becherarmin@gmail.com>
2023-10-18 21:26:32 +0200

    fix: remove make dir windows ps

--- aliasci.py
+++ aliasci.py
@@ -116,7 +116,6 @@ def generate_script(config, console_type):
         script += r'REG ADD "HKCU\Software\Microsoft\Command Processor" /t REG_SZ ' \
                   r'/v AutoRun /d c:\windows\bin\doskey.bat /f'
     elif console_type is ConsoleType.POWERSHELL:
-        script += f'\nmd {ps_setting_path} -ErrorAction SilentlyContinue \n'
         script += f'\n@"\n{aliases_cmds.replace("$", "`$")}\n"@ | Out-File -FilePath $PROFILE'
     elif console_type is ConsoleType.BASH:
         script += f"sed '/# aliasci_start/,/#aliasci_end/d' -i ~/.bashrc\n"
